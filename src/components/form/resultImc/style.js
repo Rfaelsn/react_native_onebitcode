de00529033a0b62b77c9595cd563ea918a1415c4
Rfaelsn <rafaelnascimentovf6@gmail.com>
2023-09-30 19:26:57 -0300

    feat: implementando Share. utilizando a api do react native

--- src/components/form/resultImc/style.js
+++ src/components/form/resultImc/style.js
@@ -3,7 +3,6 @@ import { StyleSheet } from "react-native";
 const styles = StyleSheet.create({
     resultImc:{
         marginTop: 1,
-        paddingTop: 60,
         borderRadius: 50,
         alignItems: "center",
         width: "100%",
@@ -17,7 +16,23 @@ const styles = StyleSheet.create({
         fontSize: 18,
         color: "#ff0043",
         fontWeight: "bold",
-    }  
+    },
+    boxShareButton:{
+        width: "100%",
+        alignItems: "center",
+        marginBottom: 10,
+    },
+    shared:{
+        backgroundColor: "#1877f2",
+        borderRadius: 50,
+        paddingBottom: 5,
+        paddingTop: 5,
+    },
+    sharedText:{
+        color: "#ffffff",
+        fontWeight: "bold",
+        paddingHorizontal: 30
+    }
 });
 
 export default styles;
